24c40281a9ea90d97002fdc28a0e5396298d3462
MinhNguyen1412 <minh.nguyen1412@asia-plus.net>
2023-04-19 16:32:00 +0700

    Add more files

---
+++ Day3.py
@@ -0,0 +1,14 @@
+#Basics Data Types:
+
+'''
+Các kiểu dữ liệu cơ bản trong Python: bool, None, int, float, str
+'''
+
+'''
+Topic #0: bool & None
+'''
+# [] bool: True & False
+var_bool = True
+
+# [] type(): Dynamically typed
+print(type(var_bool))
